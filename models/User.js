14e2cd55af09988aa907d28db4810918750446fc
himisha-G <himisha2009@gmail.com>
2026-05-06 09:18:28 +0530

    removal of user.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
 
+  role: {
+    type: String,
+    enum: ['chief_warden', 'warden', 'caretaker']
+  },
+
+  hostel: {
+    type: String,
+    enum: ['GH', 'BH1', 'BH2', 'BH3', 'BH4','BH5'],
+    required: function () {
+      return this.role !== 'chief_warden';
+    }
+  },
+
+  password: String
+});
 
 module.exports = mongoose.model('User', userSchema);
